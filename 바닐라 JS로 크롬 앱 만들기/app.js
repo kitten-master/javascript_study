6e3cb9f050698af1c7745887a3da9a0436aa2b9b
chefkitten <dgdev@naver.com>
2022-02-03 23:13:16 +0900

    calculator add

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const calculator = {
+    add: function(a,b){
+        return a+b;
+    },
+    minus: function(a,b){
+        return a-b;
+    },
+    div: function(a,b){
+        return a/b;
+    }
+};
+
+const plusNum = calculator.add(3,4);
+const minusNum = calculator.minus(3,4);
+const divNum = calculator.div(3,4);
+
+console.log(plusNum, minusNum, divNum);
