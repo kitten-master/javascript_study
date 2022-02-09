2437bd6145e90ce966dd05d90bae731a648f15ab
chefkitten <dgdev@naver.com>
2022-02-10 03:46:22 +0900

    window.addEventListener

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -1,7 +1,32 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick(){
-    title.style.color="blue";
+function handleh1Click(){
+    h1.style.color="blue";
+}
+function handleMouseEnter(){
+    h1.innerText="mouse is here";
+}
+function handleMouseLeave(){
+    h1.innerText="mouse is leave";
 }
 
-title.addEventListener("click",handleTitleClick);
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+function handleWindowCopy(){
+    alert("copier");
+}
+function handleWindowOffline(){
+    alert("no wifi");
+}
+function handleWindowOnline(){
+    alert("good");
+}
+h1.addEventListener("click",handleh1Click);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
