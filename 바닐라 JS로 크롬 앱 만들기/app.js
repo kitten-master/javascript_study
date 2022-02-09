b2631b9cba7bf7c89281c5ca1ea52be2e5865e99
chefkitten <dgdev@naver.com>
2022-02-10 02:43:09 +0900

    addEventListener

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -1,11 +1,7 @@
-/**
- * isNaN = Not a Number 숫자가 아니라면 true를 리턴, 숫자이면 false를 리턴
- */
-const age = parseInt(prompt("How old are you?"));
+const title = document.querySelector("div.hello:first-child h1");
 
-if(isNaN(age)) {
-    console.log("Please write a number");
+function handleTitleClick(){
+    title.style.color="blue";
 }
-else {
-    console.log("Thank you for writing your age.");
-}
+
+title.addEventListener("click",handleTitleClick);
