e8d6df5a59152276e1c8ce9aead64c8fbcd0244c
chefkitten <dgdev@naver.com>
2022-02-10 04:35:06 +0900

    handleh1Click

--- 바닐라 JS로 크롬 앱 만들기/app.js
+++ 바닐라 JS로 크롬 앱 만들기/app.js
@@ -1,32 +1,16 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleh1Click(){
-    h1.style.color="blue";
-}
-function handleMouseEnter(){
-    h1.innerText="mouse is here";
-}
-function handleMouseLeave(){
-    h1.innerText="mouse is leave";
+    const currentColor = h1.style.color;
+    let nowColor;
+    if(currentColor === "blue"){
+        nowColor = "tomato";
+    }
+    else{
+        nowColor = "blue";
+    }
+    h1.style.color = nowColor;
 }
 
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy(){
-    alert("copier");
-}
-function handleWindowOffline(){
-    alert("no wifi");
-}
-function handleWindowOnline(){
-    alert("good");
-}
 h1.addEventListener("click",handleh1Click);
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("online",handleWindowOnline);
+ 
